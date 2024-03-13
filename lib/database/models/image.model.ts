1b212985854bc17af6dd086da443dfdcd69d841a
SnehalMarutiSalokhe <snehal.maruti.salokhe@gmail.com>
2024-03-13 21:02:26 +0530

    webhook env var add

--- lib/database/models/image.model.ts
+++ lib/database/models/image.model.ts
@@ -4,21 +4,20 @@ export interface IImage extends Document {
   title: string;
   transformationType: string;
   publicId: string;
-  secureURL: string; 
+  secureURL: string;
   width?: number;
   height?: number;
-  config?: object; 
-  transformationUrl?: string; 
+  config?: object;
+  transformationUrl?: string;
   aspectRatio?: string;
   color?: string;
   prompt?: string;
 
-
   author: {
-        _id: string;
-        firstName: string;
-        lastName: string;
-  }
+    _id: string;
+    firstName: string;
+    lastName: string;
+  };
   createdAt?: Date;
   updatedAt?: Date;
 }
@@ -28,7 +27,7 @@ const ImageSchema = new Schema({
   transformationType: { type: String, required: true },
   publicId: { type: String, required: true },
   secureURL: { type: String, required: true },
-  
+
   width: { type: Number },
   height: { type: Number },
   config: { type: Object },
@@ -36,13 +35,11 @@ const ImageSchema = new Schema({
   aspectRatio: { type: String },
   color: { type: String },
   prompt: { type: String },
-  author: { type: Schema.Types.ObjectId, ref: 'User' },
+  author: { type: Schema.Types.ObjectId, ref: "User" },
   createdAt: { type: Date, default: Date.now },
-  updatedAt: { type: Date, default: Date.now }
+  updatedAt: { type: Date, default: Date.now },
 });
 
-const Image = models?.Image || model('Image', ImageSchema);
+const Image = models?.Image || model("Image", ImageSchema);
 
 export default Image;
-
-
